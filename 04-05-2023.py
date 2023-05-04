9f088e3f078437073b9e6eced5460b2145b00d9e
ritasylvia <impeccablesylvia@gmail.com>
2023-05-04 19:50:28 +0100

    Challenge1

--- 04-05-2023.py
+++ 04-05-2023.py
@@ -1,4 +1,16 @@
 def find_floor(instructions):
+    '''
+    This function takes in a string of instructions and returns the floor that John
+    ended up on. Each character in the string represents a move, where "<" means
+    to go up one floor and ">" means to go down one floor. John starts on the ground
+    floor (floor 0).
+    
+    Parameters:
+        instructions (str): a string of instructions for John's movements
+        
+    Returns:
+        int: the floor that John ended up on'''
+    
     floor = 0
     for char in instructions:
         if char == '<':
